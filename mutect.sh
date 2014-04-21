94de6287df9e36886eb2d59421e6b263eb8ebf07
kylec <kylelyk02@gmail.com>
2014-04-21 02:13:13 -0500

    add option to choose ref, cosmic, dbsnp version by build.

--- mutect.sh
+++ mutect.sh
@@ -1,14 +1,26 @@
 PROJ=$1
+BUILD=$2
 ANALYSISDIR=$HOME
 BAMDIR=$ANALYSISDIR/$PROJ/bam/*.bam
 SNVDIR=$ANALYSISDIR/$PROJ/mutect
 COVDIR=$ANALYSISDIR/$PROJ/cov
-#COSMIC=$ANALYSISDIR/references/chr_b37_cosmic_v54_120711.vcf
-COSMIC=$ANALYSISDIR/references/b37_cosmic_v54_120711.vcf
-#REF=$ANALYSISDIR/references/ucsc.hg19.fasta
-REF=$ANALYSISDIR/references/Homo_sapiens_assembly19.fasta
-#DBSNP=$ANALYSISDIR/references/dbsnp_132.hg19.vcf
-DBSNP=$ANALYSISDIR/references/dbsnp_132_nochr.hg19.vcf
+
+# ucsc hg19
+if [ "$BUILD" == "ucsc" ]; then
+    REF=$ANALYSISDIR/references/ucsc.hg19.fasta
+    COSMIC=$ANALYSISDIR/references/hg19_cosmic_v54_120711.vcf
+    DBSNP=$ANALYSISDIR/references/dbsnp_137.hg19.vcf
+
+# 1g ref or broad hg19 before ucsc hg19 release
+# http://gatkforums.broadinstitute.org/discussion/2226/cosmic-and-dbsnp-files-for-mutect
+elif [ "$BUILD" == "1g" ]; then
+    REF=$ANALYSISDIR/references/Homo_sapiens_assembly19.fasta
+    COSMIC=$ANALYSISDIR/references/b37_cosmic_v54_120711.vcf
+    DBSNP=$ANALYSISDIR/references/dbsnp_137.b37.vcf
+else 
+    echo "ERROR: unknown build=$BUILD."
+    exit 1
+fi
 
 cat $ANALYSISDIR/$PROJ/pairs.txt | while read PAT TSAM NSAM; do
     #echo $PAT $TSAM $NSAM
@@ -16,5 +28,7 @@ cat $ANALYSISDIR/$PROJ/pairs.txt | while read PAT TSAM NSAM; do
     NBAM=`ls $BAMDIR | grep $NSAM`;
     OUTPUTNAME=$PAT-$TSAM-$NSAM    
     echo $TBAM, $NBAM
-	java -Xmx4g -jar ~/bin/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence $REF --dbsnp $DBSNP --cosmic $COSMIC --input_file:normal $NBAM --input_file:tumor $TBAM --out $SNVDIR/$OUTPUTNAME.mutect --coverage_file $COVDIR/$OUTPUTNAME.coverage.wig.txt --vcf $SNVDIR/$OUTPUTNAME.mutect.vcf &
+	 java -Xmx4g -jar ~/bin/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence $REF --dbsnp $DBSNP --cosmic $COSMIC --input_file:normal $NBAM --input_file:tumor $TBAM --out $SNVDIR/$OUTPUTNAME.mutect --coverage_file $COVDIR/$OUTPUTNAME.coverage.wig.txt --vcf $SNVDIR/$OUTPUTNAME.mutect.vcf &
 done
+
+exit 0
