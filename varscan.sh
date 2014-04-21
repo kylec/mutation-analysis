fe7553fa4b4a0fff345c57cf9e65d72c4bf1665d
kylec <kylelyk02@gmail.com>
2014-04-21 02:15:21 -0500

    Add option for ref. Add wrapper for varscan.

--- varscan.sh
+++ varscan.sh
@@ -1,26 +1,35 @@
-PROJ=$4
-ANALYSISDIR=$HOME
-BAMDIR=$ANALYSISDIR/$PROJ/bam/*.bam
-OUTPUTDIR=$ANALYSISDIR/$PROJ/varscan
-#REF=$ANALYSISDIR/references/ucsc.hg19.fasta
-REF=$ANALYSISDIR/references/Homo_sapiens_assembly19.fasta
-
 PAT=$1 
 TSAM=$2
 NSAM=$3
+PROJ=$4
+BUILD=$5
+
+ANALYSISDIR=$HOME
+BAMDIR=$ANALYSISDIR/$PROJ/bam/*.bam
+OUTPUTDIR=$ANALYSISDIR/$PROJ/varscan
+
+if [ "$BUILD" == "ucsc" ]; then
+    REF=$ANALYSISDIR/references/ucsc.hg19.fasta
+elif [ "$BUILD" == "1g" ]; then
+    REF=$ANALYSISDIR/references/Homo_sapiens_assembly19.fasta
+else
+    echo "ERROR: unknown build=$BUILD."
+    exit 1
+fi
+
 
     #echo $PAT $TSAM $NSAM
 	TBAM=`ls $BAMDIR | grep $TSAM`;
     NBAM=`ls $BAMDIR | grep $NSAM`;
     OUTPUTNAME=$OUTPUTDIR/$PAT-$TSAM-$NSAM
     echo $TBAM, $NBAM
-    #echo `date` run samtools pileup.
-	#samtools mpileup -q 1 -f $REF $NBAM $TBAM > $OUTPUTNAME.pileup
+    echo `date` run samtools pileup.
+	samtools mpileup -q 1 -f $REF $NBAM $TBAM > $OUTPUTNAME.pileup
 
 	# fix pileup fields
     if [ "$?" == 0 ]; then
         echo `date` fix pileup fileds
-        #awk 'NF==9 && $4!=0' $OUTPUTNAME.pileup > $OUTPUTNAME.pileup.tmp && mv $OUTPUTNAME.pileup.tmp $OUTPUTNAME.pileup
+        awk 'NF==9 && $4!=0' $OUTPUTNAME.pileup > $OUTPUTNAME.pileup.tmp && mv $OUTPUTNAME.pileup.tmp $OUTPUTNAME.pileup
     else 
         echo `date` pileup failed.
     fi
