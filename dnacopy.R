92c17c39ac730a918d533a4daa4910a9ea96d174
kylec <kylelyk02@gmail.com>
2014-04-22 12:06:06 -0500

    smooth log2ratio instead of depth

--- dnacopy.R
+++ dnacopy.R
@@ -4,7 +4,7 @@ library(DNAcopy)
 
 args <- commandArgs(TRUE)
 cn <- read.table(args[1],header=T)
-CNA.object <-CNA( genomdat = cn[,6], chrom = cn[,1], maploc = cn[,2], data.type = 'logratio')
+CNA.object <-CNA( genomdat = cn[,7], chrom = cn[,1], maploc = cn[,2], data.type = 'logratio')
 CNA.smoothed <- smooth.CNA(CNA.object)
 segs <- segment(CNA.smoothed, verbose=0, min.width=2)
 segs2 = segs$output
