76d11e90dafbc7d50035afdf17ee5adb95f933ed
kylec <kylelyk02@gmail.com>
2014-04-24 18:23:03 -0500

    use author's code to normalize cnv segments

--- abscnseq.R
+++ abscnseq.R
@@ -13,35 +13,59 @@ snvFile = paste(basename, "mutect.keep.abscn_input", sep=".")
 #purity,ploidy output
 abscnFile = paste(basename, "absout", sep=".")
 
-#if(FALSE) {
-cat("read .called..")
+# read raw copynumber
 cn <- read.table(varscanFile,header=T)
-CNA.object <-CNA( genomdat = cn[,7], chrom = cn[,1], maploc = cn[,2], data.type = 'logratio')
-CNA.smoothed <- smooth.CNA(CNA.object)
-segs <- segment(CNA.smoothed, verbose=0, min.width=2)
-segs2 = segs$output
-write.table(segs2[,2:6], file=dnacopyFile, row.names=F, col.names=F, quote=F, sep="\t")
 
-cat("read dnacopy..")
-# dnacopy to abscnseq output
-d = segs2[,2:6]
-colnames(d) = c("chrom", "start","stop", "num_seg", "seg_mean")
-
-cat("sum bins..")
-# sum bins
-efflen =c()
-for (i in 1:dim(d)[1]) {
-  bins = cn[which(cn$chrom==d[i,]$chrom & cn$chr_start>=d[i,]$start & cn$chr_stop<=d[i,]$stop),]
-  efflen = append(efflen, sum(bins$chr_stop-bins$chr_start))
+# segment copynumber
+cat("segment copynumber..")
+if(file.exists(dnacopyFile)) {
+  segs = read.table(dnacopyFile, sep="\t")
+  colnames(segs) = c("chrom", "start","stop", "num_seg", "seg_mean")
+} else {
+  CNA.object <-CNA( genomdat = cn[,7], chrom = cn[,1], maploc = cn[,2], data.type = 'logratio')
+  CNA.smoothed <- smooth.CNA(CNA.object)
+  segs <- segment(CNA.smoothed, verbose=0, min.width=2)
+  segs2 = segs$output
+  write.table(segs2[,2:6], file=dnacopyFile, row.names=F, col.names=F, quote=F, sep="\t")
+  d = segs2[,2:6]
+  colnames(d) = c("chrom", "start","stop", "num_seg", "seg_mean")
 }
 
-#write output
-ratio=2^d$seg_mean
-#efflen = d$V3-d$V2
-result = cbind(d[,1:3], efflen, ratio)
-colnames(result) = c('chrom', 'loc.start', 'loc.end', 'eff.seg.len', 'normalized.ratio')
-write.table(result, file=cnFile, row.names=F, quote=F, sep="\t")
-#}
+# lei's code
+# dnacopy to abscnseq cnv input
+cat("convert segmented data..")
+
+rd.list <- split(cn,cn[,"chrom"])
+segs.list <- split(segs,segs[,"chrom"])
+cat("loop...")
+tmp.list <- list()
+n.windows <- 0
+for(i in 1:length(rd.list)) {
+  rd.df <- rd.list[[i]]
+  seg.df <- segs.list[[i]]
+  n.seg <- nrow(seg.df)
+  
+  seg.len <- integer(n.seg)
+  seg.r <- numeric(n.seg)
+  for(s in 1:n.seg) {
+    
+    seg.rd <- rd.df[rd.df[,2]>=seg.df[s,2] & rd.df[,2]<=seg.df[s,3],]  # all start location
+    
+    n.windows <- n.windows+nrow(seg.rd)
+    
+    seg.len[s] <- sum(seg.rd[,4]) # effective length without counting gaps
+    wts <- seg.rd[,4]/seg.len[s]
+    tmp.r <- seg.rd[,6]/seg.rd[,5]
+    seg.r[s] <- sum(wts*tmp.r) # normalized ratio
+    
+  }
+  
+  tmp.list[[i]] <- cbind(seg.df[,1:3],seg.len, seg.r)
+}
+
+seg.data <- do.call(rbind,tmp.list)
+colnames(seg.data) <- c("chrom", "loc.start", "loc.end", "eff.seg.len", "normalized.ratio")
+write.table(seg.data, file=cnFile, row.names=F, quote=F, sep="\t")
 
 cat("run abscnseq..")
 # run abscnseq
@@ -49,4 +73,5 @@ cat(abscnFile)
 cat(snvFile)
 my.res.list <- run.absCNSeq(cnFile, snvFile, basename, basename, seq.type="WES", min.seg.len=200)
 write.table(my.res.list$searchRes, file=abscnFile, row.names=F, quote=F, sep="\t")
-
+#plot.absCN(seg.CN, chromnum=1)
+  
