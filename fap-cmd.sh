02bfd08439eb35eba9003bbf67d93a079291b53f
kylec <kylelyk02@gmail.com>
2014-06-04 17:31:17 -0500

    1) add mutation summary. 2)add cmd on loh vs purity test

--- fap-cmd.sh
+++ fap-cmd.sh
@@ -1,3 +1,7 @@
+#### loh vs purity test on 3 samples
+for sample in Vilar12 Vilar13 Vilar22; do 
+    Rscript /Users/kyle_air/Projects/mutation-analysis/expands.R $sample > $sample.log
+done
 
 
 #### merge mutation and annotation
@@ -8,15 +12,20 @@ for a in `ls FAP*snv`; do b=`echo $a | cut -d- -f2`; mv $a $b.combined; done
 for a in `ls FAP*dnacopy`; do b=`echo $a | cut -d- -f2`; mv $a $b.copynumber.dnacopy; done
 
 #### count clones
-cd ~/projects/fap/expands
-echo -e "sample\tdominant_clone\tnum_clones\tnum_snv"
+dir=~/projects/fap/
+cd $dir/expands
+echo -e "sample\tdominant_clone\tnum_clones\tnonsyn\ton-target\traw"
 for a in `ls *.ann.tsv | cut -d. -f1`; do 
     file=$a.ann.tsv; 
+    mutect_exome=`ls $dir/mutect/$a*.exome`
+    
     # highest tumor% clone; 
     sp=`cut -f9 $file | sort -u | sort -k1n | tail -1` 
     num_sp=`cut -f9 $file | sort -u | grep -v NA | wc -l`
-    num_mut=`grep $sp $file | grep exonic | cut -f13- | grep nonsyn | wc -l`
-    echo -e "$a\t$sp\t$num_sp\t$num_mut"
+    nonsyn=`grep $sp $file | grep exonic | cut -f13- | grep nonsyn | wc -l`
+    ontarget=`grep -f<(cut -f1,2 $mutect_exome | sed 's/chr//g') $file | wc -l`
+    raw=`sed '1d' $file | wc -l`
+    echo -e "$a\t$sp\t$num_sp\t$nonsyn\t$ontarget\t$raw"
 done
 
 
@@ -24,10 +33,12 @@ done
 for a in *.tsv; do sample=`echo $a | cut -d. -f1`; max_sp=`cut -f9 $a | sort -u | egrep -v "SP|NA" | sort -k1n | tail -1`; echo -e "$sample\t$max_sp"; done
 
 
-#### filter out
-
-Kyles-MacBook-Air:expands kyle_air$ cut -f1,2 fap_mutect.report > all_var_coords
-Kyles-MacBook-Air:expands kyle_air$ cut -f1,2 fap_mutect_novel_01_May23_181211.report > novel_01_var_coords
+#### filter evs
+cut -f1,2 fap_mutect.report > all_var_coords
+cut -f1,2 fap_mutect_novel_01_May23_181211.report > novel_01_var_coords
 diff all_var_coords novel_01_var_coords  | grep "<" | cut -d" " -f2-  > excluded_var_coords
 mkdir novel_01; cd novel_01
 for a in `ls ../*combined`; do file=`echo $a | cut -d"/" -f2`;  grep -v -f excluded_var_coords $a > $file; done
+
+# rename FAP files to vilar
+for a in `ls FAP*`; do b=`echo $a | cut -d- -f2`; c=`echo $a | cut -d- -f2-`; mv $a $b-$c; done
