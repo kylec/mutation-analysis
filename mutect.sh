1035dc509281b21012d417d167e56f40ae30aa36
Kyle Chang <kchang3@cnode339.mdanderson.edu>
2014-07-18 18:53:18 -0500

    add usage. extract pairs parsing logic to submitMutect.

--- mutect.sh
+++ mutect.sh
@@ -1,6 +1,24 @@
-PROJ=$1
-BUILD=$2
-ANALYSISDIR=$HOME
+# Call mutect if .mutect output is not present
+# Filter mutect output for PASS and COVERED positions. 
+#
+# Input: Tumor/normal BAMs
+# Output: .mutect, .mutect.keep, .vcf, .pass.vcf
+#
+# usage
+# 	sh mutect.sh [project name] [build]
+# 	sh mutect.sh fap ucsc
+#  
+# kyle chang
+
+# specifically use java 1.6 
+JAVA=/scratch/rists/hpcapps/x86_64/jdk/jdk1.6.0_23/bin/java
+
+PAT=$1
+TSAM=$2
+NSAM=$3
+PROJ=$4
+BUILD=$5
+ANALYSISDIR=$6
 BAMDIR=$ANALYSISDIR/$PROJ/bam/*.bam
 SNVDIR=$ANALYSISDIR/$PROJ/mutect
 COVDIR=$ANALYSISDIR/$PROJ/cov
@@ -22,18 +40,25 @@ else
     exit 1
 fi
 
-cat $ANALYSISDIR/$PROJ/pairs.txt | while read PAT TSAM NSAM; do
-    #echo $PAT $TSAM $NSAM
-	TBAM=`ls $BAMDIR | grep $TSAM`;
-    NBAM=`ls $BAMDIR | grep $NSAM`;
-    OUTPUTNAME=$PAT-$TSAM-$NSAM    
-    echo $TBAM, $NBAM
-	if [ -f "$SNVDIR/$OUTPUTNAME.mutect" ]; then
-        echo "$SNVDIR/$OUTPUTNAME.mutect present."
+# call mutect based on pair info
+TBAM=`ls $BAMDIR | grep $TSAM`;
+NBAM=`ls $BAMDIR | grep $NSAM`;
+OUTPUTNAME=$PAT-$TSAM-$NSAM    
+if [ -f "$SNVDIR/$OUTPUTNAME.mutect" ]; then
+    echo "$SNVDIR/$OUTPUTNAME.mutect present."
+else
+    echo `date` mutect started.
+    echo "java -Xmx4g -jar ~/bin/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence $REF --dbsnp $DBSNP --cosmic $COSMIC --input_file:normal $NBAM --input_file:tumor $TBAM --out $SNVDIR/$OUTPUTNAME.mutect --coverage_file $COVDIR/$OUTPUTNAME.coverage.wig.txt --vcf $SNVDIR/$OUTPUTNAME.mutect.vcf"
+    $JAVA -Xmx4g -jar ~/bin/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence $REF --dbsnp $DBSNP --cosmic $COSMIC --input_file:normal $NBAM --input_file:tumor $TBAM --out $SNVDIR/$OUTPUTNAME.mutect --coverage_file $COVDIR/$OUTPUTNAME.coverage.wig.txt --vcf $SNVDIR/$OUTPUTNAME.mutect.vcf
+    if [ "$?" == 0 ]; then
+        # filter for pass mutations
+        sed '1d' $SNVDIR/$OUTPUTNAME.mutect | awk -F"\t" '$35!="REJECT" && $10!="UNCOVERED"' > $SNVDIR/$OUTPUTNAME.mutect.keep
+	grep -P "^#|PASS" $SNVDIR/$OUTPUTNAME.mutect.vcf > $SNVDIR/$OUTPUTNAME.mutect.pass.vcf
     else 
-        echo "$SNVDIR/$OUTPUTNAME.mutect not present."
-        java -Xmx4g -jar ~/bin/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence $REF --dbsnp $DBSNP --cosmic $COSMIC --input_file:normal $NBAM --input_file:tumor $TBAM --out $SNVDIR/$OUTPUTNAME.mutect --coverage_file $COVDIR/$OUTPUTNAME.coverage.wig.txt --vcf $SNVDIR/$OUTPUTNAME.mutect.vcf > $SNVDIR/$OUTPUTNAME.log &
+        echo "ERROR: mutect failed."
+        exit 1
     fi
-done
+fi
 
+echo `date` mutect done.
 exit 0
---
+++ submitMutect.sh
@@ -0,0 +1,28 @@
+# qsub wrapper to call mutect
+#
+# Input: project_name, build(ucsc, 1g), analysis_dir
+#
+# usage
+# 	sh submitMutect.sh [project name] [build]
+# 	sh submitMutect.sh fap ucsc
+#  
+# kyle chang
+
+
+PROJ=$1
+BUILD=$2
+#analysis dir default $HOME or some other location
+ANALYSISDIR=${3-$HOME} 
+
+if [ -f "$ANALYSISDIR/$PROJ/pairs.txt" ]; then
+    # call mutect based on pair info
+    cat $ANALYSISDIR/$PROJ/pairs.txt | while read PAT TSAM NSAM; do
+        echo "submitted:  sh $SCRIPTSDIR/mutect.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR. qsub_args: $PAT $PAT.log 8"
+        q "sh $SCRIPTSDIR/mutect.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR" $PAT $PAT.log 8
+    done
+else 
+    echo "$ANALYSISDIR/$PROJ/pairs.txt missing."
+    exit 1
+fi
+exit 0
+
