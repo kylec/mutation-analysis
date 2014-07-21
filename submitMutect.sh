47a99b122b72936ec1d16b00752a839b14ebd2dd
Kyle Chang <kylelyk02@gmail.com>
2014-07-21 18:51:19 -0500

    fix qsub params

--- submitMutect.sh
+++ submitMutect.sh
@@ -13,12 +13,14 @@ PROJ=$1
 BUILD=$2
 #analysis dir default $HOME or some other location
 ANALYSISDIR=${3-$HOME} 
+NODES=1
+PROCS=8
 
 if [ -f "$ANALYSISDIR/$PROJ/pairs.txt" ]; then
     # call mutect based on pair info
     cat $ANALYSISDIR/$PROJ/pairs.txt | while read PAT TSAM NSAM; do
-        echo "submitted:  sh $SCRIPTSDIR/mutect.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR. qsub_args: $PAT $PAT.log 8"
-        q "sh $SCRIPTSDIR/mutect.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR" $PAT $PAT.log 8
+        echo "submitted:  sh $SCRIPTSDIR/mutect.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR. qsub_args: $PAT $PAT.log $NODES $PROCS"
+        q "sh $SCRIPTSDIR/mutect.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR" $PAT $PAT.log $NODES $PROCS
     done
 else 
     echo "$ANALYSISDIR/$PROJ/pairs.txt missing."
--- submitVarscan.sh
+++ submitVarscan.sh
@@ -11,13 +11,14 @@ PROJ=$1
 BUILD=$2
 #analysis dir default home or another location
 ANALYSISDIR=${3-$HOME}
-PROC=8
+NODES=1
+PROCS=8
 
 if [ -f "$ANALYSISDIR/$PROJ/pairs.txt" ]; then
     # call mutect based on pair info
     cat $ANALYSISDIR/$PROJ/pairs.txt | head -120 | while read PAT TSAM NSAM; do
-        echo "submitted:  sh $SCRIPTSDIR/varscan.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR. qsub_args: $PAT $PAT.log $PROC"
-        q "sh $SCRIPTSDIR/varscan.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR" $PAT $PAT.log $PROC
+        echo "submitted:  sh $SCRIPTSDIR/varscan.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR. qsub_args: $PAT $PAT.log $NODES $PROCS"
+        q "sh $SCRIPTSDIR/varscan.sh $PAT $TSAM $NSAM $PROJ $BUILD $ANALYSISDIR" $PAT $PAT.log $NODES $PROCS
     done
 else
     echo "$ANALYSISDIR/$PROJ/pairs.txt missing."
