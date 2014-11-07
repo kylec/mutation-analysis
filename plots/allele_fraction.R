5368d2829f9141001e0318e4e0df0066dc8d7620
kylec <kylelyk02@gmail.com>
2014-11-07 16:32:45 -0600

    stack af and adjusted af plots

--- plots/allele_fraction.R
+++ plots/allele_fraction.R
@@ -2,7 +2,11 @@
 
 library(ggplot2)
 library(reshape2)
+library(grid)
+library(gridExtra)
 
+
+setwd("/Users/kchang3/Analysis/fap/expands/snv-only")
 # count loh after filtering
 samples = system("ls *.ann.tsv | cut -d. -f1", inter=T)
 for (i in 1:length(samples)) {
@@ -22,21 +26,55 @@ for (i in 1:length(samples)) {
   }
 }
 
+genes=c("KRAS", "APC", "GNAS", "TP53", "AKT1", "SOX9", "ARID1A", "CNOT3", "CDC27", "FBXW7", "TCF7L2", "GNAQ")
 # plot AF density/count
-plot_list = c()
+plot_list = NULL
 for (i in 1:length(samples)) {
   basename = samples[i]
-  a = read.table(paste(basename,"ann.tsv",sep="."), header=T, sep="\t")
+  a = read.table(paste0(basename,".sps.ann.tsv"), header=T, sep="\t", stringsAsFactors=F)
   head(a)
-  b = a[which(a$PN_B==0 & a$f!="NA" & a$f <= 1),]
-  b=b[grep("exonic", b$region_type),]
+  
+  
+  # allele frequency
+  b = a
+  b = b[grep("exonic", b$region_type),]
+  # keep important gene names and plot them 
+  b$region_name[b$region_name %in% genes == FALSE] = ""
+  # plot gene names with AF_Tumor (allele fraction)
   p = ggplot(b) +
-  geom_density(aes(x=AF_Tumor, fill="AF"), alpha=.3) + 
-  geom_density(aes(x=f, fill="Adj. AF"), alpha=.3) + 
-  #geom_histogram(aes(x=AF_Tumor), binwidth=.01, fill="blue", alpha=.2) +
-  #geom_histogram(aes(x=f), binwidth=.01, fill="red", alpha=.2) +
-  xlab("Allele_Fraction") + ggtitle(basename) + theme(text=element_text(size=14))
-  plot_list[[i]] = p
+    geom_histogram(aes(x=AF_Tumor), binwidth=.01, fill="blue", alpha=.2) +
+    geom_density(aes(x=AF_Tumor), alpha=.2) + 
+    scale_x_continuous(limits=c(0,1)) +
+    scale_y_continuous(limits=c(0,15)) + 
+    xlab("Allele_Fraction") + ggtitle(basename) + theme(text=element_text(size=14)) + 
+    geom_text(aes(x=AF_Tumor, y=1, label=region_name, angle=90))
+  p
+  
+  # absolute AF
+  b = a
+  b = b[which(b$PN_B==0 & b$f!="NA" & a$f <= 1),]
+  b = b[grep("exonic", b$region_type),]
+  b$region_name[b$region_name %in% genes == FALSE] = ""
+  
+  # number of clones
+  sp = length(sort(unique(a$SP[!is.na(a$SP)])))
+  
+  p1 = ggplot(b) +
+    geom_histogram(aes(x=f), binwidth=.01, fill="red", alpha=.2) +
+    geom_density(aes(x=f), alpha=.2) + 
+    scale_x_continuous(limits=c(0,1)) +
+    scale_y_continuous(limits=c(0,15)) + 
+    xlab("Absolute Allele_Fraction") + ggtitle(basename) + theme(text=element_text(size=14)) + 
+    geom_text(aes(x=f, y=1, label=region_name, angle=90))
+  p1
+   
+  #g = arrangeGrob(p)
+  g = arrangeGrob(p,p1)
+  grid.arrange(g, ncol=1)
+  # save image
+  ggsave(paste0(basename, ".af.png"), g, width=10, height=15)
+  
+  #plot_list[[i]] = p
   # using melt
   #c=cbind(b$AF_Tumor,b$f)
   #colnames(c)=c('a','b')
@@ -44,8 +82,8 @@ for (i in 1:length(samples)) {
   #d=melt(c)
   #ggplot(d, aes(x=value, fill=Var2)) + geom_density(alpha = .3)
 }
-library(grid)
-library(gridExtra)
+
+# plot all together
 names(plot_list) = samples
 png("test2.png", width=2000, height=2000)
 arg_list <- c(plot_list, list(nrow=4, ncol=4))
