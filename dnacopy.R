203e975fb4d581b1a6055d8b02adca04e3b64c1c
Kyle Chang <kylelyk02@gmail.com>
2014-12-16 16:51:18 -0600

    create dnacopy output with p values for varscan mergerSegments.pl

--- dnacopy.R
+++ dnacopy.R
@@ -9,3 +9,6 @@ CNA.smoothed <- smooth.CNA(CNA.object)
 segs <- segment(CNA.smoothed, verbose=0, min.width=2)
 segs2 = segs$output
 write.table(segs2[,2:6], file=args[2], row.names=F, col.names=F, quote=F, sep="\t")
+# write p values for segment to be used in varscan mergeSegments.pl
+pseg = segments.p(segs, ngrid=100, tol=1e-6, alpha=0.05, search.range=100, nperm=1000)
+write.table(pseg, file=args[3], row.names=F, col.names=T, quote=F, sep="\t")
