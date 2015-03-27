92caa8b108e2b0aec9fec422c654a9894a5d178a
Kyle Chang <kylelyk02@gmail.com>
2015-03-27 11:34:03 -0500

    add varscan path option and output dir

--- absolute.sh
+++ absolute.sh
@@ -13,16 +13,24 @@ NSAM=$3
 PROJ=$4
 BUILD=$5
 ANALYSISDIR=$6
-OUTPUTDIR=absolute
+VARSCAN=$7
+OUTPUTDIR=$8
+
 SNVDIR=$ANALYSISDIR/$PROJ/mutect
-CNVDIR=$ANALYSISDIR/$PROJ/varscan
+CNVDIR=$ANALYSISDIR/$PROJ/$VARSCAN
 ABSDIR=$ANALYSISDIR/$PROJ/$OUTPUTDIR
 
 if [ "$BUILD" == "ucsc" ]; then
     REF="hg19"
+elif [ "$BUILD" == "1g" ]; then
+    REF="GRCh37.75"
+else
+    echo "ERROR:build==$BUILD not recognized."
+    exit 1
 fi
 
 # check mutect and cnv files exist
+#VCF=`ls $SNVDIR/*.pass.exome.vcf | grep $PAT`
 VCF=`ls $SNVDIR/*.pass.vcf | grep $PAT`
 CNV=`ls $CNVDIR/*.called.dnacopy | grep $PAT`
 if [ "$VCF" == "" ] || [ "$CNV" == "" ]; then
@@ -30,6 +38,8 @@ if [ "$VCF" == "" ] || [ "$CNV" == "" ]; then
 fi
 
 # absolute input 
+#VCF_EFF=`echo $VCF | sed 's/pass.exome/pass.exome.snpeff/'`
+#MAF=`echo $VCF | sed 's/pass.exome.vcf/pass.exome.snpeff.maf/'`
 VCF_EFF=`echo $VCF | sed 's/pass/pass.snpeff/'`
 MAF=`echo $VCF | sed 's/pass.vcf/pass.snpeff.maf/'`
 SEG=$CNV.seg
@@ -46,14 +56,14 @@ fi
 # VCF to MAF
 if [ "$?" == 0 ]; then
     if [ -f "$MAF" ]; then		
+        echo "`date` $MAF present."
+    else
         # remove chrX,Y,M, gl
         echo "`date` Remove chrX,Y,M, gl and chr prefix"
         awk '{FS=OFS="\t"; if ($1 ~ /#/ || $1 !~ /M|X|Y|GL|gl|hap/) { print $0}}' $VCF_EFF | sed 's/^chr//g' > $VCF_EFF.tmp && mv $VCF_EFF.tmp $VCF_EFF
         echo "`date` Convert vcf to maf."
         echo "perl $VCF2MAF_HOME/vcf2maf.pl --input-snpeff $VCF_EFF --output-maf $MAF --tumor-id $TSAM --normal-id $NSAM"
         perl $VCF2MAF_HOME/vcf2maf.pl --input-snpeff $VCF_EFF --output-maf $MAF --tumor-id $TSAM --normal-id $NSAM
-    else
-        echo "`date` $MAF present."
     fi
 else
     echo "ERROR: snpeff error."; exit 1
@@ -64,7 +74,7 @@ if [ "$?" == 0 ]; then
     echo -e "Chromosome\tStart\tEnd\tNum_Probes\tSegment_Mean" > $SEG && cat $CNV >> $SEG
     echo "`date` Start absolute."
     echo "Rscript $SCRIPTSDIR/absolute.R $ANALYSISDIR $PROJ $OUTPUTDIR $MAF $SEG $PAT $REF"
-    Rscript $SCRIPTSDIR/absolute.R $ANALYSISDIR $PROJ $OUTPUTDIR $MAF $SEG $PAT 
+    Rscript $SCRIPTSDIR/absolute.R $ANALYSISDIR $PROJ $OUTPUTDIR $MAF $SEG $PAT $REF 
 
 else
     echo "ERROR: vcf2maf failed."; exit 1
