f004303de5dbddbe7a94606f5986a63c879ab046
Kyle Chang <kylelyk02@gmail.com>
2017-08-05 16:25:58 -0500

    add header. remove pairned end switch, check if there's just one fastq instead

--- rnaseq/submitRsemExpression.py
+++ rnaseq/submitRsemExpression.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python
+#
+# submit rsem transcriptome count
+#
+# usage: 
+# python2 sumbitRsemExpression.py [fastq path] [sample name] [rsem ref] [proc]
+# python2 submitRsemExpression.py \"../source_data/Sample_H_G$i/*.fastq\" Sample_H_G$i \"$HOME/references/rsem_resources/hg19\" 16"
+
 import sys
 import glob
 import os
 from subprocess import check_call, call
 import re
 
+# arguments
 inputFastqFilePattern = sys.argv[1]
 outputPrefix = sys.argv[2]
 indexBaseDirectory = sys.argv[3]	#rsem reference $HOME/references/rsem_resources/hg19
 procs = sys.argv[4]
 
-# single end switch 
-single_end = sys.argv[5]
-
-sampleNames = set()
 # gets list of files based on the pattern passed at command line.
 dirList = glob.glob(inputFastqFilePattern)
-#print dirList
 fastqFilesR1 = []
 fastqFilesR2 = []
 for fastq in dirList:
-	if re.search('_R1_', fastq):
+	if re.search("_R1_|_r1|\.1\.", fastq):
 		fastqFilesR1.append(fastq)
 	else:
+		# assume single end read if doens't match for R1|r1
 		fastqFilesR2.append(fastq)
 
 
@@ -30,17 +34,18 @@ for fastq in dirList:
 fastqFilesR1 = sorted(fastqFilesR1)
 fastqFilesR2 = sorted(fastqFilesR2)
 
-#print "fastq1"
-#print fastqFilesR1
-#print "fastq2"
-#print fastqFilesR2
+print "fastq1 files."
+print fastqFilesR1
+print "fastq2 files."
+print fastqFilesR2
 
 # rsem-calculate-expression -p [procs] --paired-end [fastq1] [fastq2]
 print "running rsem-calculate expression."
-print single_end
-if single_end == '1':
+if len(fastqFilesR1) == 0:
+	print "single end"
 	cmd = 'rsem-calculate-expression -p ' + procs + ','.join(fastqFilesR1) + ' ' + ','.join(fastqFilesR2) + ' ' + indexBaseDirectory + ' ' + outputPrefix
 else:
+	print "paired end"
 	cmd = 'rsem-calculate-expression -p ' + procs + ' --paired-end ' + ','.join(fastqFilesR1) + ' ' + ','.join(fastqFilesR2) + ' ' + indexBaseDirectory + ' ' + outputPrefix
 
 print cmd
