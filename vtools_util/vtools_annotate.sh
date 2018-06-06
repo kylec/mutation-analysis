3a7a88b9e3e3309d1ef6bf6193e8b3032ba0739c
Kyle Chang <kylelyk02@gmail.com>
2018-06-06 18:21:10 -0500

    initial commit

---
+++ vtools_util/vtools_annotate.sh
@@ -0,0 +1,17 @@
+vtools use refGene-hg19_20130904
+vtools use CancerGeneCensus-20130711 --linked_by refGene.name2
+vtools use dbSNP-hg19_138
+vtools use evs-6500 --as evs
+vtools use thousandGenomes-hg19_20130502 --as thousandGenomes
+vtools use ExAC-hg19_r0.2
+vtools use dbNSFP
+vtools use CosmicCodingMuts-v67_20131024 --as CosmicCodingMuts
+vtools use CosmicMutantExport-v67_241013 --linked_by CosmicCodingMuts.COSMIC_ID
+vtools use CosmicNonCodingVariants-v67_241013
+vtools use ccdsGene-hg19_20130904
+vtools use keggPathway-20110823 --linked_by ccdsGene.name
+vtools export variant --output ANNOVAR.input --format ANNOVAR
+ANNOVAR=/rsrch2/epi/scheet/TEAM_ROOT/3rdparty/no-arch/annovar-2016feb01/
+perl $ANNOVAR/annotate_variation.pl -geneanno ANNOVAR.input -buildver hg19 $ANNOVAR/humandb/
+vtools update variant --format ANNOVAR_exonic_variant_function --from_file ANNOVAR.input.exonic_variant_function --var_info mut_type function genename
+vtools update variant --format ANNOVAR_variant_function --from_file ANNOVAR.input.variant_function --var_info region_type region_name
